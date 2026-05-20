ea2fdd79664b539e3841a866c2fc2facc5294889
dasari chandrasekhar <dasarichandrasekhar23@gmail.com>
2026-05-20 11:31:49 +0530

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,68 @@
+'''type coversions
+--------------
+'''
+'''an = 34
+print(type(an))
+gh=str(an)
+print(type(gh))
+jk=float(an)
+print(type(jk)
+
+float--->
+
+car=978
+print(int(car))
+print(type(str(car)))
+cg=str(car)
+print(cg)
+print(type(cg))
+
+list--->
+any=[4,5,6]
+print(str(any))
+print(len(any))
+
+int as  a user-input
+-------------------
+ghj=int(input("enter a number"))
+sd=(34+ghj)
+print(sd)
+
+str as a user-input
+-----------------
+jkl=input("write u r name")
+
+
+
+fsa=list(map(int,input("enter the number :").split()))
+print(fsa)
+ 
+fsa=tuple(map(int,input("enter the number :").split()))
+print(fsa)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
