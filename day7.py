2279b7e81a76f43f9df6d1f3e0399ae747a5badf
dasari chandrasekhar <dasarichandrasekhar23@gmail.com>
2026-05-21 15:43:05 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,98 @@
+''' DAY 6'''
+'''f=int(input("enter \n1.red \n2.green \noption:"))
+if f==1:
+    print("green")
+else:
+    print("red")'''
+'''marks=int(input("enter your marks : "))
+if marks>=90:
+    print("a+")
+elif marks>=80:
+    print("a")
+elif marks>=70:
+    print("b")
+elif marks>=60:
+    print("c")
+elif marks>=50:
+    print("pass")
+else:
+    print("fail")'''
+'''
+a=int(input())
+b=int(input())
+c=int(input())
+if a > b and a>c:
+    print(a)
+elif b>c and b>a:
+    print(b)
+else: 
+    print(c)'''
+
+'''nested if'''
+'''
+sbi_bank={"atm pin":"6600"}
+pin =input("enter 4 digit atm pin :")
+if len(str(pin))==4:
+    if pin in sbi_bank['atm pin']:
+        print(" welcome to sbi atm")
+    else:
+        print("invaild pin")
+else:
+    print("incorect atm pin")'''
+
+''' for statement
+-----------------
+
+-->
+used to itterate over a squence
+dg="chandrasekhar"
+for joy in dg:
+    print(joy)
+'''
+'''
+range()
+----------
+----> range is in built function use to generate number in squance manner
+syntax--> range(start,end,step)
+
+
+else in for
+------------
+---> once the itteasions completed this else will be
+
+break
+-----
+---->
+used to exit from the loop based on the condition
+
+continue
+--------
+----> used to skip current itteration base on the condition
+
+pass
+----
+---> 
+
+
+'''
+'''
+for i in range(1,100):
+    print(i)
+else:
+    print("stop")'''
+
+''' while
+    -----
+--->for+if
+
+
+
+
+
+
+
+
+
+
+
+
