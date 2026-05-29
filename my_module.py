e53d0b66cf1374a6a3ede5ceb93201d1176fbff0
dasari chandrasekhar <dasarichandrasekhar23@gmail.com>
2026-05-29 10:04:56 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,64 @@
+#modules
+#-------
+'''
+--> a modules in python is a file that contatins python code such as
+--variables
+--classes
+--statements
+'''
+#two types of modules
+#-------------------
+'''
+user-define
+built-in
+'''
+'''
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a+b)
+'''
+'''
+import math
+print(math.sqrt(25))
+print(math.factorial(12))
+print(math.pow(2,3))
+'''
+'''
+from math import sqrt
+print(int(sqrt(36)))
+'''
+'''
+import math as m
+print(m.factorial(10))
+print(m.pow(2,3))
+'''
+'''
+import os
+os.mkdir("hii.txt")
+os.rmdir("hii.txt")
+'''
+'''
+import sys
+print(sys.version)
+print(sys.path)
+'''
+'''
+import random
+print(random.randint(1000,9999))
+'''
+'''
+from collections import Counter
+data=['a','v','s','g','f']
+counter = Counter(data)
+print(counter)
+'''
+'''
+from collections import Counter,defaultdict
+data=['a','v','s','g']
+counter = Counter(data)
+print(counter)
+dd= def'''
+'''
+
+'''
