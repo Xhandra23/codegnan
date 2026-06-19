0c966a8f1ae903354e2e1cb13e96b731fe0eb1f9
dasari chandrasekhar <dasarichandrasekhar23@gmail.com>
2026-06-19 10:18:56 +0530

    Add files via upload

---
+++ web scraping project.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+URL = "http://books.toscrape.com/"
+
+try:
+    response = requests.get(URL)
+    response.encoding = "utf-8"
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
+    exit()
+
+# Parse HTML
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find all books
+books = soup.find_all("article", class_="product_pod")
+
+book_names = []
+book_prices = []
+
+for book in books:
+    name = book.h3.a["title"]
+
+    price_text = book.find("p", class_="price_color").text
+    price = float(re.findall(r"\d+\.\d+", price_text)[0])
+
+    book_names.append(name)
+    book_prices.append(price)
+
+# Create DataFrame
+df = pd.DataFrame({
+    "Book_Name": book_names,
+    "Price": book_prices
+})
+
+print(df)
+print("Shape:", df.shape)
+print(df.head())
+
+# Save CSV
+df.to_csv("books_data.csv", index=False)
+print("CSV File Saved Successfully!")
+
+# Plot
+plt.figure(figsize=(10, 5))
+plt.bar(df["Book_Name"][:10], df["Price"][:10])
+
+plt.xticks(rotation=90)
+plt.xlabel("Book Names")
+plt.ylabel("Price (£)")
+plt.title("Top 10 Book Prices")
+plt.tight_layout()
+
+plt.show()
